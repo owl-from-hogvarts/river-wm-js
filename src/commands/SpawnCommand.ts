86ecf6aff19a265d04ba100b58b3339f5cd0847f
owl-from-hogvarts <transftank@mail.ru>
2023-04-29 22:37:52 +0300

    fix: properly implement `ICommand` for `SpawnCommand`

--- src/commands/SpawnCommand.ts
+++ src/commands/SpawnCommand.ts
@@ -1,13 +1,16 @@
+import { BaseCommand } from "./BaseCommand";
 import { ICommand } from "./ICommand";
 
-export class SpawnCommand implements ICommand {
+export class SpawnCommand extends BaseCommand implements ICommand {
 
   constructor(
     private readonly execCommand: string,
     public readonly name: string,
-  ) {}
+  ) {
+    super()
+  }
 
-  readonly command: string = "spawn"
+  override readonly command: string = "spawn"
   get args() {
     return [this.execCommand]
   }
