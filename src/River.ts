db547f198a3d9c8b1cd10388b664caf18bbb3309
owl-from-hogvarts <transftank@mail.ru>
2023-04-29 21:15:08 +0300

    feat: make `riverOptions` type

--- src/River.ts
+++ src/River.ts
@@ -4,17 +4,17 @@ import { MapCommand } from "./commands/mapCommand/MapCommand";
 import { Mode } from "./commands/mapCommand/keyBindings/Mode";
 
 class River {
-  private readonly commands: ICommand[] = []
-  
+  private readonly commands: ICommand[] = [];
+
   constructor(private readonly executer: IExecuter, modes: Mode[]) {
-    this.buildKeyBindingCommands(modes)
+    this.buildKeyBindingCommands(modes);
   }
 
   private buildKeyBindingCommands(modes: Mode[]) {
-    const commands: ICommand[] = []
-    
+    const commands: ICommand[] = [];
+
     for (const mode of modes) {
-      commands.push(...MapCommand.commandsForMode(mode))
+      commands.push(...MapCommand.commandsForMode(mode));
     }
   }
 
@@ -23,7 +23,34 @@ class River {
    */
   public apply() {
     for (const command of this.commands) {
-      this.executer.execute(command)
+      this.executer.execute(command);
     }
   }
 }
+
+enum AttachMode {
+  TOP,
+  BOTTOM,
+}
+
+type RiverOptions = Partial<{
+  attachMode: AttachMode;
+  theme: Partial<{
+    borderWidth: number;
+    borderColorFocused: string;
+    borderColorUnfocused: string;
+    borderColorUrgent: string;
+    cursor: {
+      cursorTheme: string;
+      size: number;
+    };
+    backgroundColor: string;
+  }>;
+  focusFollowsCursor: boolean;
+  /** Hide cursor when typing or after inactivity */
+  hideCursor: boolean;
+  repeat: {
+    rate: number;
+    delay: number;
+  };
+}>;
