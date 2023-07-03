04d473b38152fc458602eeb4fd67693ae7c099a5
owl-from-hogvarts <transftank@mail.ru>
2023-07-03 09:04:07 +0300

    feat: aggregate interface for all features

--- src/object-model/River.ts
+++ src/object-model/River.ts
@@ -1,15 +1,37 @@
+import { ICanEnterMode } from "../actions/EnterMode";
+import { ICanFocus } from "../actions/Focus";
+import { ICanMove } from "../actions/Move";
+import { ICanSpawn } from "../actions/Spawn";
+import { ICanSwap } from "../actions/Swap";
+import { ICanToggleFloat } from "../actions/ToggleFloat";
 import { Color } from "./Color";
 import { BaseMode, SwitchableMode } from "./keyBindings/Mode";
 
-export type RiverModesDefinition = {
-  DEFAULT_MODE?: BaseMode;
-  LOCK_MODE?: BaseMode;
-  otherModes: SwitchableMode[]
+export type FullFeatures<T> = ICanEnterMode<T> &
+  ICanFocus<T> &
+  ICanMove<T> &
+  ICanSpawn<T> &
+  ICanSwap<T> &
+  ICanToggleFloat<T>;
+
+export type RiverModesDefinition<T> = {
+  DEFAULT_MODE?: BaseMode<T>;
+  LOCK_MODE?: BaseMode<T>;
+  otherModes: SwitchableMode<T>[];
 };
 
-export class River {
+// aggregate interfaces defined by actions
+// into single type. Implementation then provides
+// something which complies the interface.
+// So, when new action is added:
+// 1. action itself is created (mostly data)
+// 2. interface with visitor method is created
+// 3. interface is added to aggregated type
+// 4. implementations are updated to comply new interface
+
+export class River<Features> {
   constructor(
-    public readonly modes: RiverModesDefinition,
+    public readonly modes: RiverModesDefinition<Features>,
     public readonly options: RiverOptions
   ) {}
 }
